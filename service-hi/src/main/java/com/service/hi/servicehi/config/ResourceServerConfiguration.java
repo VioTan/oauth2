4dc1e75264774bd649f327eb14ed81adef20fb76
Eric <981524709@qq.com>
2022-02-15 18:11:39 +0800

    test

--- service-hi/src/main/java/com/service/hi/servicehi/config/ResourceServerConfiguration.java
+++ service-hi/src/main/java/com/service/hi/servicehi/config/ResourceServerConfiguration.java
@@ -15,7 +15,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 @EnableGlobalMethodSecurity
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
+    /**
+     * 添加EnableResourceServer注解开启资源服务的功能，加注解EnableGlobalMethodSecurity开户方法级别的保护，
+     * ResourceServerConfigurerAdapter是配置类，configure(HttpSecurity http)中只配置了"/order/**"需要验证。
+     * @param http
+     * @throws Exception
+     */
     @Override
     public void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests().antMatchers("/order/**").authenticated();// 配置order访问控制，必须认证后才可以访问
